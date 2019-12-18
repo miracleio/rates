34146d15bfa696cdaf293622f18e2c691e8fd527
Hiroutes Digital <miracleficient@gmail.com>
2019-12-18 17:46:53 +0100

    UI update - added a feed post UI to better illustrate the rate functionality

--- script.js
+++ script.js
@@ -1,43 +1,51 @@
-
+const limit = 10;
 const docStyles = document.documentElement.style;
 const actionBtn = document.querySelector("#action-btn");
 const ratesDisp = document.querySelector("#count");
-const limit = 15;
-let rates = sessionStorage.getItem("rates");
-let height = ratesDisp.children[ratesDisp.children.length - 1].clientHeight;
 const message = document.querySelector("#message");
 
+let rates = sessionStorage.getItem("rates");
+let totalrates = localStorage.getItem("totalrates");
+let height = ratesDisp.children[ratesDisp.children.length - 1].clientHeight;
 let tx = 0;
 
-const checkSession = ()=>{
+const checkStorage = ()=>{
     if( sessionStorage.getItem("rates") == null){
         sessionStorage.setItem("rates", 0);
     };
-    rated();
-    setDisp();
-};
-
-const checkLocal = ()=>{
-    if( localStorage.getItem("totalrates") == null){
+    if( localStorage.getItem("totalrates") == null || localStorage.getItem("totalrates") == 0){
         localStorage.setItem("totalrates", 0);
+        sessionStorage.setItem("rates", 0);
     };
+    totalrates = localStorage.getItem("totalrates");
+    rates = sessionStorage.getItem("rates");
+
+    rated();
 };
 
 const setDisp = ()=>{
-    ratesDisp.firstElementChild.textContent = rates;
+    ratesDisp.firstElementChild.textContent = totalrates;
+    if(rates >= limit) actionBtn.classList.add("rated");
+    height = ratesDisp.children[ratesDisp.children.length - 1].clientHeight;
+    tx += height;
+
+    var newVal = document.createElement("span");
+    newVal.textContent = totalrates;
+    ratesDisp.appendChild(newVal);
+
+    docStyles.setProperty("--tx", `-${tx}px`);
+    docStyles.setProperty("--height", `${height}px`);
 }
 
 const rate = ()=>{
+    checkStorage();
     if(parseInt(rates) >= limit){
         limitReached();
         return false
     }
     rateFunc();
-    console.log(rates);
+    console.log({rates: rates, total: totalrates});
     sessionStorage.setItem("rates", rates);
-    let totalrates = parseInt(localStorage.getItem("totalrates"));
-    totalrates ++;
-    console.log(totalrates);
     localStorage.setItem("totalrates", totalrates);
 };
 
@@ -51,6 +59,7 @@ const rated = ()=>{
 }
 
 const limitReached = ()=>{
+    rated();
     message.textContent = `Alright! You can't rate more than ${limit}`;
     message.parentElement.classList.add("show");
     actionBtn.classList.add("rated");
@@ -61,20 +70,14 @@ const limitReached = ()=>{
 
 const rateFunc = ()=>{
     rates++;
-    if(rates >= limit) actionBtn.classList.add("rated");
-    height = ratesDisp.children[ratesDisp.children.length - 1].clientHeight;
-    tx += height;
 
-    console.log(rates, tx);
+    totalrates++;
+    setDisp();
+    console.log({translateX : tx});
 
-    var newVal = document.createElement("span");
-    newVal.textContent = rates;
-    ratesDisp.appendChild(newVal);
 
-    docStyles.setProperty("--tx", `-${tx}px`);
-    docStyles.setProperty("--height", `${height}px`);
 }
 
-window.addEventListener("load", checkSession);
-window.addEventListener("load", checkLocal);
+window.addEventListener("load", checkStorage);
+window.addEventListener("load", setDisp);
 actionBtn.addEventListener("click", rate);
